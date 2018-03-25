b04c7a62641155e512d32cd258208cc5f5f99555
Kun Hong <chingu100@gmail.com>
2018-03-25 18:45:44 +0900

    intial commit collections example

---
+++ src/main/java/com/company/collections/ArrayListExample.java
@@ -0,0 +1,12 @@
+package com.company.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Java");
+        list.add("Servelet/JSP");
+    }
+}
